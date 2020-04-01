9733c0f10b475ba29d282dfd74457301d56e21a3
Oxibug <agskitkat@gmail.com>
2020-04-01 15:09:51 +0300

    Авторизация

--- resources/views/menus/main.blade.php
+++ resources/views/menus/main.blade.php
@@ -29,10 +29,15 @@
                     Профиль
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="{{ route('admin.article.add') }}">Добавить статью</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        Выйти
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
             @endguest
 
